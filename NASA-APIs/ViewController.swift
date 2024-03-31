ba9fd11ff6ec9767f613cbed925e67e5f91a8e10
Eduard Latynskii <lokolat88@gmail.com>
2024-03-31 16:01:43 +0500

    initial commit

---
+++ NASA-APIs/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  NASA-APIs
+//
+//  Created by Eduard on 31.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
